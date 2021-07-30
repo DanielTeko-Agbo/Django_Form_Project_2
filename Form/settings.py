7d5b93e4b72273bcae854401a2ac1f8f0109ac2c
Teiko-Agbo Daniel <agbodaniel531.gmail.com>
2021-07-30 09:52:54 -1200

    another commit

--- Form/settings.py
+++ Form/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'Form.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [r'C:\Users\owner\Desktop\FormProject\Form\Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
